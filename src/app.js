72de7a8ab576cba422593a40a83ebda878db2414
Augusto <augustobrito@outlook.com>
2020-07-31 17:00:22 -0300

    get post ids

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 
-const IG_URL = 'https://www.instagram.com/';
+// Download
+import axios from 'axios';
+import fs from 'fs';
+
+const IG_URL = 'https://www.instagram.com';
 const IG_LOAD_MORE_CLASS = '.dCJp8';
-const IG_POST_URL_CLASS = '.v1Nh3';
+const IG_POST_URL_CLASS = '.v1Nh3 a';
+
+let user;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,41 +17,54 @@ const rl = readline.createInterface({
 });
 
 rl.question('What is your instagram profile? \n', (answer) => {
-  let user = answer; // TODO: Validate before set
+  user = answer; // TODO: Validate before set
   console.log(`OK ${user}, starting...`);
   rl.close();
 });
 
 rl.on('close', () => {
   console.log('Closed!');
-  startBrowserProcess();
+  startBrowserProcess().then((browser) => createPage(browser));
   //process.exit(0);
 });
 
 async function startBrowserProcess() {
   console.log('Launching Browser...');
   const browser = await puppeteer.launch();
-
-  console.log('Creating Page...');
-  const page = await browser.newPage();
-
-  console.log(`Navigating to: ${IG_URL}`);
-  await page.goto(IG_URL);
-
-  console.log('Selecting Load more comments Button...');
-  const button = await page.$(`${IG_LOAD_MORE_CLASS}`);
-
-  if (button) {
-    await button.click(() => {
-      console.log('clicked!');
-    });
-  }
-
-  browser.close();
+  return browser;
 }
 
-//  - Digitar @perfil IG
-// Selecionar IDs dos Posts Disponiveis
-// Contar e Ordenar @ marcados nos comentarios do Post
-//	Exemplo: @julio: 3x @carlos 2x @joao
-// Mostrar
+async function stopBrowserProcess(browser) {
+  try {
+    console.log('Closing Browser...');
+    browser.close();
+    browser = null;
+    page = null;
+  } catch (error) {
+    console.log('Error closing Browser: ', error);
+  }
+}
+
+async function createPage(browser) {
+  let url = `${IG_URL}/${user}`;
+
+  //TODO: Validate URL before create a page
+  console.log(`Creating Page for: ${url}`);
+  const page = await browser.newPage();
+
+  console.log(`Going to Page: ${url}`);
+  await page.goto(url);
+
+  const hrefs = await page.$$eval(IG_POST_URL_CLASS, (elements) =>
+    elements.map((element) => element.href)
+  );
+
+  const ids = hrefs.map((href) => href.slice(28, href.length - 1));
+  console.log(ids);
+
+  await stopBrowserProcess(browser);
+}
+
+async function download(url, name, extension) {
+  //https://github.com/ScriptSmith/instamancer/blob/master/src/http/download.ts
+}
